652fde438111feec3773ca3dbb855b5c09447f44
Just_AirOn <143044665+HardGamerAron@users.noreply.github.com>
2025-12-13 23:29:11 +0100

    Boldog Karacsonyt Ervin

---
+++ feladat3.py
@@ -0,0 +1,8 @@
+szam = 10
+
+while szam >= 1:
+    if szam % 2 == 1:
+        print(szam)
+        szam -= 1
+    else:
+        szam -= 1
---
+++ feladat4.py
@@ -0,0 +1,6 @@
+szoveg = input("kerek egy szoveget ")
+szam = int(input("Mennyiszer ird ki a szoveget?"))
+alap = 0 
+while alap < szam:
+    print(szoveg)
+    alap = alap + 1
